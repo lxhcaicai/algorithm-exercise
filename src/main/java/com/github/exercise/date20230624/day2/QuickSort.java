ef003a129f5db30a4e498762e6b589ce81b19ba0
lxhcaicai <2778763221@qq.com>
2023-06-24 19:23:40 +0800

    [day2] add quicksort

---
+++ src/main/java/com/github/exercise/date20230624/day2/QuickSort.java
@@ -0,0 +1,39 @@
+package com.github.exercise.date20230624.day2;
+
+import java.util.Scanner;
+
+public class QuickSort {
+
+    final static int N = (int) (1e5 + 100);
+    static int n = 0;
+    static int a[] = new int[N];
+
+    static void qsort(int l, int r) {
+        int mid = a[(l + r) >> 1], i = l, j = r;
+        while(i < j) {
+            while(a[i] < mid) i ++;
+            while(a[j] > mid) j --;
+            if(i <= j) {
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                i ++;
+                j --;
+            }
+        }
+        if(i < r) qsort(i, r);
+        if(l < j) qsort(l, j);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        for(int i = 1; i <= n; i ++) {
+            a[i] = scanner.nextInt();
+        }
+        qsort(1, n);
+        for(int i = 1; i <= n; i ++) {
+            System.out.printf("%d ", a[i]);
+        }
+    }
+}
