f23042b40373ed666667435b9a03b60094eb5436
lxhcaicai <2778763221@qq.com>
2023-06-24 19:35:59 +0800

    [day2] add 最短Hamilton路径

---
+++ src/main/java/com/github/exercise/date20230624/day2/Hamilton.java
@@ -0,0 +1,38 @@
+package com.github.exercise.date20230624.day2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hamilton {
+
+    static final int N = 20;
+    static int dis[][] = new int[N][N];
+    static int f[][] = new int[1 << N][N];
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < n; j ++) {
+                dis[i][j] = scanner.nextInt();
+            }
+        }
+        for(int i = 0; i < (1 << N); i ++) {
+            Arrays.fill(f[i], 0x3f3f3f3f);
+        }
+        f[1][0] = 0;
+        for(int i = 1; i < (1 << n); i ++) {
+            for(int j = 0; j < n; j ++) {
+                if((i >> j & 1) == 1) {
+                    for(int k = 0; k < n; k ++) {
+                        if(((i ^ (1 << j)) >> k & 1) == 1) {
+                            f[i][j] = Integer.min(f[i][j], f[i ^ (1 << j)][k] + dis[k][j]);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(f[(1 << n) - 1][n - 1]);
+    }
+}
